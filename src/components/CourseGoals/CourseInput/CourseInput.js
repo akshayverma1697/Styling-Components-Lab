bcc5ef54ffbdf89e35666cf3ae8294106841772f
Akshay Verma <47255769+akshayverma1697@users.noreply.github.com>
2023-08-15 22:27:29 -0500

    added template literal div FormControl and comments to explain how their color schemas are being changed

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+/* tagged template literal for form control div, 
+lines 17, 23, and 24 will change the color schema 
+depending on the pointer function "invalid" */
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -23,13 +54,14 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* Line 27 will set the boolean isValid to true or false 
-        based on the arguments in goalInputHandler and will then 
-        dynamically style the Input Label and text field */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* Line 59 will implement the tagged template literal "FormControl"
+      and then use a pointer function "invalid" to change between colors 
+      depending on whether or not the boolean isValid is true or false
+      depending on user input after clicking the add goal button*/}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
